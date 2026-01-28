be73d43f47cca41f73dedf9a43a330bd7e9e0891
Солтанов Александр Назимович <a.soltanov@k2t.app>
2026-01-28 22:29:57 +0300

    fix

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/sh
+set -eu
 
 echo "[run.sh] Starting service"
 
 echo "[run.sh] Running DB migrations"
-goose -dir ./db/migrations postgres "${DATABASE_URL}" up
+goose -dir ./db/migrations postgres "${DATABASE_URL:?DATABASE_URL is required}" up
 
 echo "[run.sh] Starting Go app"
 exec /app/bin/app
