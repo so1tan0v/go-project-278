c6a6bf742250a94e9d61032ef2e906fc9550c436
Солтанов Александр Назимович <a.soltanov@k2t.app>
2026-01-30 21:24:12 +0300

    init caddy

--- bin/run.sh
+++ bin/run.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
-set -eu
+#!/usr/bin/env bash
+set -euo pipefail
 
 echo "[run.sh] Starting service"
 
 echo "[run.sh] Running DB migrations"
-goose -dir ./db/migrations postgres "${DATABASE_URL:?DATABASE_URL is required}" up
+goose -dir ./db/migrations postgres "${DATABASE_URL}" up
+
+echo "[run.sh] Starting Caddy"
+caddy run --config /etc/caddy/Caddyfile &
 
 echo "[run.sh] Starting Go app"
-exec /app/bin/app
+exec env PORT=8080 /app/bin/app
